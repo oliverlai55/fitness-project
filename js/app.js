711d4979fc5a2b57fb1b77f409ccec0aaf643021
Oliver Lai <oliverlai55@gmail.com>
2016-01-26 01:05:22 -0500

    updates to app js and index

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
-function ShopCtrl($scope){
+function ShopCtrl($scope) {
 
 	$scope.items = [
-		{id: 1, text: 'Bench Press', bought: false},
-		{id: 2, text: 'Incline Fly', bought: true},
-		{id: 3, text: 'Decline Barbell Bench Press', bought: false}
+		{id: 1, text: 'Cheese - 200g', bought: false},
+		{id: 2, text: 'Bread', bought: true},
+		{id: 3, text: 'Onions x4', bought: false}
 	];
 
-	$scope.clearBought = function(){
-		$scope.items = _.filter($scope.items, function(item){
+	$scope.getTotalItems = function() {
+		return $scope.items.length;
+	}
+
+	$scope.clearBought = function() {
+		$scope.items = _.filter($scope.items, function(item) {
 			return !item.bought;
 		});
 	}
 
-	$scope.addItem = function(){
-		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1)});
+	$scope.addItem = function() {
+		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
 		$scope.itemEntry = '';
 	}
+
+	$scope.isBought = function(bought) {
+		return (bought) ? 'bought' : 'not-bought';
+	}
+
+	$scope.itemSlug = function(text) {
+		return text.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+	}
+
 }
