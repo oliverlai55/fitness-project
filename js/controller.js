c56bf2c821b8b4ce9d4e4df9611a7bfa44b2d5d6
Oliver Lai <oliverlai55@gmail.com>
2016-02-01 20:09:16 -0500

    generated default workout list

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,18 @@
 angular.module('myApp', []).controller('myController', function($scope){
 
-function House(name, type, sets, reps, breaktime){
+function List(name, type, sets, reps, breaktime){
 	this.name = name;
 	this.type = type;
 	this.sets = sets;
 	this.reps = reps;
 	this.breaktime = breaktime;
 }
+
+var listArray = [];
+listArray.push(new List("Flat Barbell Bench Press","Chest","4","10-15","1 mins"));
+listArray.push(new List("Incline Dumbbell Bench Press","Chest","3","12-15","1 mins"));
+listArray.push(new List("Decline Barbell Bench Press","Chest","4","12","1 mins"));
+
+$scope.lists = listArray;
+
+});
