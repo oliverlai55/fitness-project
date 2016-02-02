6558bb13095ca2c164025cc71d44af2ab7272fbb
Oliver Lai <oliverlai55@gmail.com>
2016-02-01 20:02:40 -0500

    added more angular

--- js/controller.js
+++ js/controller.js
@@ -1,33 +1,9 @@
 angular.module('myApp', []).controller('myController', function($scope){
 
-
-	$scope.items = [
-		{id: 1, text: 'Bench Press', bought: false},
-		{id: 2, text: 'Dumbbell Fly', bought: true},
-		{id: 3, text: 'Inclined Bench Press', bought: false}
-	];
-
-	$scope.getTotalItems = function() {
-		return $scope.items.length;
-	}
-
-	$scope.clearBought = function() {
-		$scope.items = _.filter($scope.items, function(item) {
-			return !item.bought;
-		});
-	}
-
-	$scope.addItem = function() {
-		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
-		$scope.itemEntry = '';
-	}
-
-	$scope.isBought = function(bought) {
-		return (bought) ? 'bought' : 'not-bought';
-	}
-
-	$scope.itemSlug = function(text) {
-		return text.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
-	}
-
-});
+function House(name, type, sets, reps, breaktime){
+	this.name = name;
+	this.type = type;
+	this.sets = sets;
+	this.reps = reps;
+	this.breaktime = breaktime;
+}
